dd61da9bd280cb6dd8b1c4d60f2a04f22ba9717a
RuixiJiang <jiangruixi630@gmail.com>
2022-01-10 15:05:49 -0500

    initial commit

---
+++ shopify/src/test/java/com/demo/shopify/ShopifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.shopify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
